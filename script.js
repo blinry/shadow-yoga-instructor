a8736d4d7c1e569b6c25ac33ea4e093f590985ef
Sebastian Morr <sebastian@morr.cc>
2019-08-29 16:17:43 +0200

    Thresholded version

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const player = document.getElementById("player")
-const canvas = document.getElementById("canvas")
-const context = canvas.getContext("2d")
+
+const canvasOriginal = document.getElementById("original")
+const contextOriginal = canvasOriginal.getContext("2d")
+
+const canvasThreshold = document.getElementById("threshold")
+const contextThreshold = canvasOriginal.getContext("2d")
+
 const captureButton = document.getElementById("capture")
 
 const constraints = {
@@ -9,7 +14,53 @@ const constraints = {
 
 captureButton.addEventListener("click", () => {
     // Draw the video frame to the canvas.
-    context.drawImage(player, 0, 0, canvas.width, canvas.height)
+    contextOriginal.drawImage(
+        player,
+        0,
+        0,
+        canvasOriginal.width,
+        canvasOriginal.height,
+    )
+
+    var brightPixels = 0
+
+    var frame = contextOriginal.getImageData(
+        0,
+        0,
+        canvasOriginal.width,
+        canvasOriginal.height,
+    )
+    var data = frame.data
+    var length = frame.data.length / 4
+
+    for (i = 0; i < length; i++) {
+        var r = data[i * 4 + 0]
+        var g = data[i * 4 + 1]
+        var b = data[i * 4 + 2]
+        var a = data[i * 4 + 3]
+
+        var brightness = (r + g + b) / 3
+        //console.log(i)
+        //console.log(brightness)
+
+        if (brightness > 100) {
+            r = 255
+            g = 255
+            b = 255
+            a = 255
+        } else {
+            r = 0
+            g = 0
+            b = 0
+            a = 255
+        }
+
+        data[i * 4 + 0] = r
+        data[i * 4 + 1] = g
+        data[i * 4 + 2] = b
+        data[i * 4 + 3] = a
+    }
+    contextThreshold.putImageData(frame, 0, 0)
 })
 
 // Attach the video stream to the video element and autoplay.
