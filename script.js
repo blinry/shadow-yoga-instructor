ebf9373e4ea986a13479c555c530720ff4d66cf6
Sebastian Morr <sebastian@morr.cc>
2019-08-29 16:22:54 +0200

    Automatic update

--- script.js
+++ script.js
@@ -12,7 +12,41 @@ const constraints = {
     video: true,
 }
 
-captureButton.addEventListener("click", () => {
+function threshold(data, level) {
+    var length = data.length / 4
+
+    for (i = 0; i < length; i++) {
+        var r = data[i * 4 + 0]
+        var g = data[i * 4 + 1]
+        var b = data[i * 4 + 2]
+        var a = data[i * 4 + 3]
+
+        var brightness = (r + g + b) / 3
+        //console.log(i)
+        //console.log(brightness)
+
+        if (brightness > level) {
+            r = 255
+            g = 255
+            b = 255
+            a = 255
+        } else {
+            r = 0
+            g = 0
+            b = 0
+            a = 255
+        }
+
+        data[i * 4 + 0] = r
+        data[i * 4 + 1] = g
+        data[i * 4 + 2] = b
+        data[i * 4 + 3] = a
+    }
+
+    return data
+}
+
+function updateImage() {
     // Draw the video frame to the canvas.
     contextOriginal.drawImage(
         player,
@@ -30,40 +64,14 @@ captureButton.addEventListener("click", () => {
         canvasOriginal.width,
         canvasOriginal.height,
     )
-    var data = frame.data
-    var length = frame.data.length / 4
 
-    for (i = 0; i < length; i++) {
-        var r = data[i * 4 + 0]
-        var g = data[i * 4 + 1]
-        var b = data[i * 4 + 2]
-        var a = data[i * 4 + 3]
-
-        var brightness = (r + g + b) / 3
-        //console.log(i)
-        //console.log(brightness)
-
-        if (brightness > 100) {
-            r = 255
-            g = 255
-            b = 255
-            a = 255
-        } else {
-            r = 0
-            g = 0
-            b = 0
-            a = 255
-        }
-
-        data[i * 4 + 0] = r
-        data[i * 4 + 1] = g
-        data[i * 4 + 2] = b
-        data[i * 4 + 3] = a
-    }
+    threshold(frame.data, 100)
     contextThreshold.putImageData(frame, 0, 0)
-})
+}
 
 // Attach the video stream to the video element and autoplay.
 navigator.mediaDevices.getUserMedia(constraints).then(stream => {
     player.srcObject = stream
 })
+
+setInterval(updateImage, 100)
